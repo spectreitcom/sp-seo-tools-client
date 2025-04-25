3feecf93e6d0743b8222014af9e821b30ad35a31
Przemyslaw Chudzinski <p.chudzinski.spectreit@gmail.com>
2025-04-25 09:03:27 +0200

    SST2-224

--- src/components/pages/pages-list.tsx
+++ src/components/pages/pages-list.tsx
@@ -1,5 +1,6 @@
 import { PageData } from "../../hooks";
 import { ArrowTopRightOnSquareIcon } from "@heroicons/react/16/solid";
+import Badge from "../ui/badge.tsx";
 
 type Props = Readonly<{
   data: Omit<PageData, "factors">[];
@@ -13,10 +14,15 @@ function PagesList({ data }: Props) {
           href={item.url}
           target="_blank"
           key={item.pageId}
-          className={"flex p-2 text-indigo-600 hover:font-semibold"}
+          className={"flex p-2"}
         >
           <div className={"w-16 text-black"}>{item.position}</div>
-          <div className={"flex items-center"}>
+          {item.position === 0 && (
+            <Badge color={"info"} text={"Your site"} className={"mr-2"} />
+          )}
+          <div
+            className={"flex items-center text-indigo-600 hover:font-semibold"}
+          >
             <span className={"mr-2"}>{item.url}</span>
             <ArrowTopRightOnSquareIcon className={"size-5"} />
           </div>
