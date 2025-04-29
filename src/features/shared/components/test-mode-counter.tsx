c65956e76cdf7cd69eee583515d4a94a7f7b12ca
Przemyslaw Chudzinski <p.chudzinski.spectreit@gmail.com>
2025-04-29 16:11:36 +0200

    improved

--- src/features/shared/components/test-mode-counter.tsx
+++ src/features/shared/components/test-mode-counter.tsx
@@ -35,9 +35,12 @@ function TestModeCounter({ expiresAt, label }: Props) {
 
   if (!expiresAt) return null;
   return (
-    <div>
-      {label}
-      <span className={"ml-2 font-semibold text-indigo-600"} ref={counterRef} />
+    <div className="flex items-center space-x-2 bg-gray-100 p-4 rounded-lg shadow-sm">
+      {label && <span className="text-gray-700 font-medium">{label}</span>}
+      <span
+        className="ml-2 font-semibold text-indigo-600 text-lg"
+        ref={counterRef}
+      />
     </div>
   );
 }
